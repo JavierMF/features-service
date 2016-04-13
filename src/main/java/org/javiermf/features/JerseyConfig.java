515f44794d0f9631e934ab4669e03e24a33f7826
Javier Muñoz Ferrara <javier.munoz@gmail.com>
2016-04-13 21:08:56 +0200

    Project migrated to use Jersey instead of SpingMVC

---
+++ src/main/java/org/javiermf/features/JerseyConfig.java
@@ -0,0 +1,13 @@
+package org.javiermf.features;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.javiermf.features.services.rest.ProductResource;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig() {
+        register(ProductResource.class);
+    }
+}
+
--- src/main/java/org/javiermf/features/services/rest/ProductResource.java
+++ src/main/java/org/javiermf/features/services/rest/ProductResource.java
@@ -6,24 +6,25 @@ import org.javiermf.features.models.Configuration;
 import org.javiermf.features.models.Feature;
 import org.javiermf.features.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@RestController
-@RequestMapping("/products")
+@Component
+@Path("/products")
+@Produces("application/json")
 public class ProductResource {
 
     @Autowired
     ProductsDAO productsDAO;
 
-    @RequestMapping(method = GET)
+    @GET
     public List<String> getAllProducts() {
         List<String> allProducts = new ArrayList<String>();
 
@@ -34,13 +35,15 @@ public class ProductResource {
         return allProducts;
     }
 
-    @RequestMapping(value = "/{productName}", method = RequestMethod.GET)
-    public Product getProductByName(@PathVariable String productName) {
+    @Path("{productName}")
+    @GET
+    public Product getProductByName(@PathParam("productName") String productName) {
         return productsDAO.getProductByName(productName);
     }
 
-    @RequestMapping(value = "/{productName}/configurations", method = RequestMethod.GET)
-    public List<String> getConfigurationsForProduct(@PathVariable String productName) {
+    @Path("{productName}/configurations")
+    @GET
+    public List<String> getConfigurationsForProduct(@PathParam("productName") String productName) {
         List<String> configurationsForProduct = new ArrayList<String>();
 
         for (Configuration configuration : productsDAO.getConfigurationsForProduct(productName)) {
@@ -50,13 +53,17 @@ public class ProductResource {
         return configurationsForProduct;
     }
 
-    @RequestMapping(value = "/{productName}/configurations/{configurationName}", method = RequestMethod.GET)
-    public Configuration getConfigurationWithNameForProduct(@PathVariable String productName, @PathVariable String configurationName) {
+    @Path("{productName}/configurations/{configurationName}")
+    @GET
+    public Configuration getConfigurationWithNameForProduct(@PathParam("productName") String productName,
+                                                            @PathParam("configurationName") String configurationName) {
         return productsDAO.getConfigurationWithNameForProduct(productName, configurationName);
     }
 
-    @RequestMapping(value = "/{productName}/configurations/{configurationName}/features", method = RequestMethod.GET)
-    public List<String> getConfigurationActivedFeatures(@PathVariable String productName, @PathVariable String configurationName) {
+    @Path("{productName}/configurations/{configurationName}/features")
+    @GET
+    public List<String> getConfigurationActivedFeatures(@PathParam("productName") String productName,
+                                                        @PathParam("configurationName") String configurationName) {
         Configuration configuration = productsDAO.getConfigurationWithNameForProduct(productName, configurationName);
         List<String> featureNames = new ArrayList<String>();
         for (Feature feature : configuration.getActivedFeatures()) {
@@ -64,7 +71,6 @@ public class ProductResource {
         }
         return featureNames;
     }
-
 
 
 
