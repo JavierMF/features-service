ef8412c8d00813053f9865a72b12400f1d464860
Javier Muñoz Ferrara <javier.munoz@gmail.com>
2016-04-15 08:18:44 +0200

    Changing test to @WebIntegrationTests so the webapp runs when in integrations tests

--- src/test/java/org/javiermf/features/services/rest/ProductResourceTests.java
+++ src/test/java/org/javiermf/features/services/rest/ProductResourceTests.java
@@ -5,8 +5,8 @@ import org.javiermf.features.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.List;
 
@@ -17,7 +17,7 @@ import static org.springframework.test.util.MatcherAssertionErrors.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@WebIntegrationTest
 public class ProductResourceTests {
 
     @Test
